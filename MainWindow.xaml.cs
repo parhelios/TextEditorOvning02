49c53ab88aa72e96430c8e33eab3d1f3f077e051
niklas-hjelm <niklas.hjelm@iths.se>
2022-10-12 09:43:57 +0200

    Update after Demo

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,37 +11,9 @@ namespace TextEditor
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        private TextProps _textProps = new ();
-
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = _textProps;
-        }
-
-        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            OpenFileDialog dlg = new OpenFileDialog();
-            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
-            if (dlg.ShowDialog() == true)
-            {
-                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
-                TextRange range = new TextRange(TextField.Document.ContentStart, TextField.Document.ContentEnd);
-                range.Load(fileStream, DataFormats.Rtf);
-            }
-        }
-
-        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            SaveFileDialog dlg = new SaveFileDialog();
-            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
-            if (dlg.ShowDialog() == true)
-            {
-                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
-                TextRange range = new TextRange(TextField.Document.ContentStart, TextField.Document.ContentEnd);
-                range.Save(fileStream, DataFormats.Rtf);
-            }
         }
     }
 }
